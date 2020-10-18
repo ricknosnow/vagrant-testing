34162a59e946c222995c975d52b0dd74aa9116ff
Rick Davidson <rd_187@yahoo.com>
2020-10-18 12:05:57 -0400

    vaulting working

--- runansible.sh
+++ runansible.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ansible-playbook -u vagrant -i .vagrant/provisioners/ansible/inventory/vagrant_ansible_inventory bootstrap.yml
+ansible-playbook -u vagrant --vault-password-file ~/.vault_key -i .vagrant/provisioners/ansible/inventory/vagrant_ansible_inventory bootstrap.yml
